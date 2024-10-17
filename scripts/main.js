dcd2b2c8498a4042690f715554c90cb976a519fc
Z2212 <ortnerzoe@gmail.com>
2024-10-17 17:23:53 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,3 @@
 gsap.registerPlugin(ScrollTrigger);
 
 
-gsap.to('.miniBiker', {
-    x: '100vw',
-   
-    scrollTrigger: {
-        markers: true,
-        start: 'top center',
-        end: 'bottom 30%',
-        trigger: '.miniBiker',
-        scrub: 10,
-        pin: true,
-    }
-}
-
-
-);
